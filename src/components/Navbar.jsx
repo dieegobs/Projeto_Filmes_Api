3a0309680ba4f337fbf985e2d9b142f85ce582fc
Diego Bittencourt <diegohevant@gmail.com>
2023-01-16 18:23:36 -0300

    Listagem dentro da pagina do filme

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,17 +1,39 @@
-import { Link } from "react-router-dom";
-import { BiCameraMovie, BiSearchAlt2 } from "react-icons/bi";
+import { useState } from "react";
 
+import { Link, useNavigate } from "react-router-dom";
+import { BiCameraMovie, BiSearchAlt2 } from "react-icons/bi";
+import "./Navbar.css";
 
 const Navbar = () => {
+
+    const [search, setSearch] = useState("");
+    const navigate = useNavigate();
+
+    const handleSubmit = (e) => {
+        e.preventDefault()
+    
+        if(!search) return
+
+        navigate(`/search?q=${search}`);
+        setSearch("");
+
+    };
+
+
     return (
         <nav id="navbar">
             <h2>
                 <Link to="/">
-                     <BiCameraMovie /> MoviesLib 
+                    <BiCameraMovie /> MoviesLib
                 </Link>
             </h2>
-            <form>
-                <input type="text" name="" id="" placeholder="Busque um Filme" />
+            <form onSubmit={handleSubmit}>
+                <input type="text"
+                    name="" id=""
+                    placeholder="Busque um Filme"
+                    onChange={(e) => setSearch(e.target.value)}
+                    value={search}
+                />
                 <button type="submit">
                     <BiSearchAlt2 />
                 </button>
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,4 +1,8 @@
 import { useState, useEffect } from "react";
+import MovieCard from "../components/MovieCard";
+
+import "./MoviesGrid.css"
+
 
 const moviesURL = import.meta.env.VITE_API;
 const apiKey = import.meta.env.VITE_API_KEY;
@@ -22,18 +26,24 @@ const Home = () => {
     
     getTopRatedMovies(topRateUrl);
 
-  }, [])
+  }, []);
 
 
   console.log(moviesURL, apiKey);
 
   return (
-    <div>
+    
 
-    {topMovies && topMovies.map((movie) => <p>{movie.title}</p>)}
-
+    <div className="container">
+      <h2 className="title">Melhores filmes:</h2>
+      <div className="movies-container">
+        {topMovies.length === 0 && <p>Carregando</p>}
+      {topMovies.length > 0 && topMovies.map((movie) => <MovieCard key={movie.id} movie={movie} />)}
+      </div>
     </div>
-  )
-}
+
+   
+  );
+};
 
 export default Home
