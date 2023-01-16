a7d2a45e3dd910cbe0233ba1ec8719289a712158
Diego Bittencourt <diegohevant@gmail.com>
2023-01-16 12:28:06 -0300

    Puxando a listagem de títulos

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,6 +1,38 @@
+import { useState, useEffect } from "react";
+
+const moviesURL = import.meta.env.VITE_API;
+const apiKey = import.meta.env.VITE_API_KEY;
+
+
+
 const Home = () => {
+
+  const [topMovies, setTopMovies] = useState([])
+
+  const getTopRatedMovies = async (url) => {
+    const res = await fetch(url);
+    const data = await res.json();
+
+    setTopMovies(data.results);
+  };
+
+  useEffect(()=> {
+    
+    const topRateUrl = `${moviesURL}top_rated?${apiKey}&language=pt-BR`;
+    
+    getTopRatedMovies(topRateUrl);
+
+  }, [])
+
+
+  console.log(moviesURL, apiKey);
+
   return (
-    <div>Home</div>
+    <div>
+
+    {topMovies && topMovies.map((movie) => <p>{movie.title}</p>)}
+
+    </div>
   )
 }
 
