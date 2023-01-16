5ae21c665475fb1ca8b50e53ded905a291bf1e8c
Diego Bittencourt <diegohevant@gmail.com>
2023-01-16 18:44:47 -0300

    Projeto Finalizado listagem, detalhes e search

--- src/pages/Movie.jsx
+++ src/pages/Movie.jsx
@@ -29,14 +29,55 @@ const Movie = () => {
        setMovie(data);
       }
 
+
+        const formatCurrency = (number) => {
+          return number.toLocaleString("en-US", {
+            style: "currency",
+            currency: "USD"
+          });
+        };
+
+
       useEffect(() => {
         const movieUrl = `${moviesURL}${id}?${apiKey}`
         getMovie(movieUrl)
       }, [])
 
-       return <div>
+       return <div className="movie-page">
 
-        {movie && <>{movie.title}</>}
+        {movie && 
+        <>
+        <MovieCard movie={movie} showLink={false}/>
+      
+      <p className="tagline">{movie.tagline}</p>
+      <div className="info">
+        <h3>
+          <BsWallet2 />Orçamento:
+        </h3>
+        <p>{formatCurrency(movie.budget)}</p>
+      </div>
+
+      <div className="info">
+        <h3>
+          <BsGraphUp />Receita:
+        </h3>
+        <p>{formatCurrency(movie.revenue)}</p>
+      </div>
+      <div className="info">
+        <h3>
+          <BsHourglassSplit />Duração:
+        </h3>
+        <p>{movie.runtime}</p>
+      </div>
+      <div className="info description">
+        <h3>
+          <BsFillFileEarmarkTextFill />Descrição:
+        </h3>
+        <p>{movie.overview}</p>
+      </div>
+        
+        </>
+        }
 
        </div>
      
